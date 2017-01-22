863fcb5b5d139bd1f232dc8d855a0a4ad6985b6a
loic laureote <loic.laureote@arkena.com>
2017-01-22 18:57:01 +0100

    add enveloppe node

---
+++ echopen-leaderboard/enveloppe_node/submit_function.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def normImag(A):
+    A = A - A.min()
+    A = 1.0*A/A.max()
+    return(A)
+    
+def reconstructImage(rawSignal,image_shape) :
+    # Here is a copy of the baseline method. Replace that by another method.
+    reconstructedImage = np.zeros(shape=(image_shape[0],image_shape[1]))
+    decimationFactor = 1.0*rawSignal.shape[0]/image_shape[0]
+
+    for i in range(rawSignal.shape[0]):
+           for j in range(image_shape[1]):
+                reconstructedImage[int(i/decimationFactor)][j] += np.abs(rawSignal[i][j])
+
+    reconstructedImage = normImag(np.abs(reconstructedImage))
+    # The function should return the reconstructed image
+    return reconstructedImage
