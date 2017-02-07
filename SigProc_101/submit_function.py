df2ca4010e356e46494fa39713b5e65314d319bf
loic laureote <loic.laureote@arkena.com>
2017-02-07 23:18:03 +0100

    mod on SigProc_101

--- SigProc_101/submit_function.py
+++ SigProc_101/submit_function.py
@@ -1,11 +1,8 @@
-import numpy as np
-
-def normImag(A):
-    A = A - A.min()
-    A = 1.0*A/A.max()
-    return(A)
+def install_packages():
+    import pip
+    pip.main(['install', 'numpy'])
     
-def reconstructImage(rawSignal,image_shape) :
+def run(rawSignal,image_shape) :
     # Here is a copy of the baseline method. Replace that by another method.
     reconstructedImage = np.zeros(shape=(image_shape[0],image_shape[1]))
     decimationFactor = 1.0*rawSignal.shape[0]/image_shape[0]
@@ -13,7 +10,10 @@ def reconstructImage(rawSignal,image_shape) :
     for i in range(rawSignal.shape[0]):
            for j in range(image_shape[1]):
                 reconstructedImage[int(i/decimationFactor)][j] += np.abs(rawSignal[i][j])
-
-    reconstructedImage = normImag(np.abs(reconstructedImage))
+    
+    A = np.abs(reconstructedImage)
+    A = A - A.min()
+    A = 1.0*A/A.max()
+    reconstructedImage = A    
     # The function should return the reconstructed image
     return reconstructedImage
